cabaf49dda0c028f8a3d825f90c2de0b57f45b2b
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2025-12-10 02:44:06 +0200

    fix test:e2e:report

--- client/cypress.config.ts
+++ client/cypress.config.ts
@@ -1,10 +1,23 @@
 import { defineConfig } from "cypress";
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
 
 export default defineConfig({
-  e2e: {
-    baseUrl: "http://localhost:5173",
-    setupNodeEvents() {
+   e2e: {
+    baseUrl: 'http://localhost:5173',
+    supportFile: false,
+    reporter: process.env.CYPRESS_REPORTER || 'spec',
+    reporterOptions: {
+      reportDir: 'cypress/reports',
+      overwrite: true,
+      html: true,
+      json: false
+    },
+    setupNodeEvents(on) {
+      if (process.env.CYPRESS_REPORTER === 'cypress-mochawesome-reporter') {
+        require('cypress-mochawesome-reporter/plugin')(on);
+      }
       // implement node event listeners here
     },
   },
-});
+})
