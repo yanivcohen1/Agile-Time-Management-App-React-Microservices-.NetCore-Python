d9ef2aa8644e2fd4c9f1a30354ab6c6afc820c70
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2025-12-15 11:25:25 +0200

    update tslint configuration

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -19,5 +19,11 @@ export default defineConfig([
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    rules: {
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/no-explicit-any': 'error',
+      'react-hooks/exhaustive-deps': 'error',
+      'no-console': 'warn',
+    },
   },
 ])
